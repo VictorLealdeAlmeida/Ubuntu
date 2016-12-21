8f8871a2b3c16a715371251147e7280e5ff6b57a
VictorLealdeAlmeida <vlpaa@cin.ufpe.br>
2016-12-21 15:15:25 -0300

    Initial Commit

---
+++ Ubuntu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ubuntu
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 21/12/16.
+//  Copyright © 2016 VTR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
