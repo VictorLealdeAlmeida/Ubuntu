9404aeec0edd92793649e7a08a729c03810a7f8c
VictorLealdeAlmeida <vlpaa@cin.ufpe.br>
2016-12-26 19:37:09 -0300

    Novas fontes, backgrouns, mudando conteudo com o touch

--- Ubuntu/ViewController.swift
+++ Ubuntu/ViewController.swift
@@ -10,14 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var textView: UIView!
+    @IBOutlet weak var titleView: UIView!
+    
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var titleMusic: UILabel!
+    @IBOutlet weak var author: UILabel!
+    
+    var color = [UIColor(red:0.70, green:0.39, blue:0.09, alpha:1.00),
+                 UIColor(red:0.73, green:0.18, blue:0.16, alpha:1.00),
+                 UIColor(red:0.16, green:0.10, blue:0.01, alpha:1.00),
+                 UIColor(red:0.19, green:0.44, blue:0.57, alpha:1.00),
+                 UIColor(red:0.54, green:0.30, blue:0.08, alpha:1.00),
+                 UIColor(red:0.00, green:0.01, blue:0.20, alpha:1.00),
+                 UIColor(red:0.16, green:0.08, blue:0.02, alpha:1.00),
+                 UIColor(red:0.78, green:0.49, blue:0.18, alpha:1.00)]
+    var backNumber = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textView.layer.cornerRadius = 15
+        titleView.layer.cornerRadius = 15
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        changeBackground()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return UIStatusBarStyle.lightContent;
+    }
+    
+    func changeBackground(){
+        background.image = UIImage(named:"afro"+String(backNumber))
+        text.textColor = color[backNumber-1]
+        titleMusic.textColor = color[backNumber-1]
+        author.textColor = color[backNumber-1]
+        
+        if backNumber < 8{
+            backNumber += 1
+        }else{
+            backNumber = 1
+        }
     }
 
 
